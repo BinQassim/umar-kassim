a4450084a8535a76e85e505c77968fe3219c6844
BinQassim <81852965+BinQassim@users.noreply.github.com>
2021-04-04 18:06:47 +0100

    Add files via upload

---
+++ CSC .PY.py
@@ -0,0 +1,41 @@
+ #a program that display name and department
+name = "umar kasim umar"
+department = "computer science"
+print ("name",":",name)
+print ("department",":",department)
+
+#a program to display boo
+scare = "Boo!"
+print (scare)
+
+#a program to display addition of variables
+a = 'maimuna'
+b = ' is a mumu'
+result = a + b
+print (result)
+
+#a program that divides 12/ .5
+k = 12
+u = 0.5
+g = k/u
+print(g)
+
+#a program to assign sum of two numbers to a variable and print the variable
+h = 12
+j = 5
+k = h + j
+print (k)
+
+#a program that finds the remainder when 8 is divided by 3
+g = 8
+h = 3
+v = g % h
+print (v)
+
+#Rewrite the following statement to force this order: First, multiply 2 by 4. Then add 4 and 2. Then multiply the first result by the second result. Print the result.
+d = 2
+f = 4
+t = d * f
+o = f + d
+k = t * o
+print (k)
